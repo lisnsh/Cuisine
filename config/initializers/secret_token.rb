fce166ef356c71af52ecb9da40751620acd434bd
lisnsh <lian.winfa@gmail.com>
2013-10-16 23:58:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuisine::Application.config.secret_token = '1ed0453ff5e30ee1cc04fab6fc2f1af66c50206105235e675ba704dbc5ebc1ce0f8dd1e20a1b3cb6d20b230e0dcd119d7fd6958193b37622ffb1a8c3af2f5e11'
